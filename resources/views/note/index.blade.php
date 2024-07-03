237e338a88fa47a8f545bb9ce04d51cbe9855e37
Tony <jayton006@gmail.com>
2024-07-03 11:14:12 +0300

    view and styling

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,21 +1,30 @@
 <x-layout>
     <div class="note-container">
+        <!-- new note btn -->
         <a href=" {{ route('note.create') }} " class="new-note-btn">
-           <h1>New Note heading</h1>
-           <p>a para for notes index view</p>
+           <h1><b>New Note btn</b></h1>
         </a>
+
+        <!-- note div -->
         <div class="notes">
             @foreach($notes as $note)
+            <!-- note body -->
             <div class="note">
                 <div class="note-body">
                     {{ Str::words($note-> note, 30) }}
                 </div>
+                <br>
+                <!-- note action buttons -->
                 <div class="note-buttons">
                     <a href="{{ route('note.show', $note) }}" class="note-edit-button">View</a>
                     <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
                     <button class="note-delete-button">Delete</button>
                 </div>
             </div>
+            <br>
+            <br>
+            <br>
+            <br>
             @endforeach
         </div>
     </div>
