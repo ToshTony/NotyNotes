f73cf586c90b9fbb2d12b99642143d5287571453
Tony <jayton006@gmail.com>
2024-07-26 11:50:30 +0300

    dates and routes

--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -1,7 +1,30 @@
 <x-app-layout>
     <div class="note-container single-note">
         <div class="note-header">
-            <h1 class="text-3xl py-4">Note: {{ $note->created_at }}</h1>
+            <!-- normal date -->
+            <h1 class="text-3xl py-4">Date Created at: {{ $note->created_at}}... {{ $note->created_at->diffForHumans()}}</h1>
+
+             <!-- human readable, created xx minutes ago -->
+             <h1 class="text-3xl py-4">Note Created at: {{ $note->created_at->diffForHumans()}}</h1>
+
+             <p>Current date and time: {{ date('Y-m-d H:i:s') }}</p>
+
+             <p>12Created At: {{ $note->created_at->format('l, F j, Y \a\t g:i A') }}</p>
+
+             <p>24Created At: {{ $note->created_at->format('l, F j, Y \a\t H:i') }}</p>
+
+
+            @php
+                $userTimezone = 'America/New_York'; // Example timezone
+                $createdAt = $note->created_at->timezone($userTimezone);
+            @endphp
+
+            <p>Created At ({{ $userTimezone }}): {{ $createdAt->format('F j, Y g:i A') }}</p>
+
+
+
+
+
             <div class="note-buttons">
                 <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
                 <form action="{{ route('note.destroy', $note) }}" method="POST">
